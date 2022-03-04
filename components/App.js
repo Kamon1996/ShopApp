c1fbf6c38875852ff34e5eab44fd13d557868a82
Dzmitriy Kalhanau <96485499+Kamon1996@users.noreply.github.com>
2022-03-04 21:33:05 +0300

    Update App.js

--- components/App.js
+++ components/App.js
@@ -2,6 +2,8 @@ import { main } from "./Main.js";
 import { busket } from "./Busket.js";
 import { product } from "./Product.js";
 import { getCookie, setCookie } from "./common.js";
+import { displayMain, displayBusket, displayProduct } from "./Display.js";
+import { hashChange, getDinHash } from "./RemoteHash.js";
 
 export class App {
   constructor() {
@@ -19,6 +21,7 @@ export class App {
     } else {
       this.busket = []; // общий массив со всеми товарами в корзине
     }
+    this.remoteConfig = [];
   }
   create() {
     this.totalCostBusket = document.createElement("div");
@@ -53,117 +56,33 @@ export class App {
     }
   }
 
-  getMain() {
-    main.innerHTML = ``;
-    this.prodcuts.forEach((e) => {
-      main.innerHTML += `
-        <div id="${e.id}" class="main__item">
-          <a id="${e.id}" href="#product/${e.id}"><i class="fas fa-info"></i></a>
-          <img src="${e.image}" alt="" />
-          <h4 class="item__title">${e.title}</h4>
-          <h5 class="item__price">${e.price} $</h5>
-          <div class="item__about">
-            <h6>
-              rating:
-              <p>${e.rating.rate}</p>
-            </h6>
-            <h6>
-              count:
-              <p>${e.rating.count}</p>
-            </h6>
-
-              <button id="${e.id}" class="to__busket-add">Add to busket</button>
-              <div class="to__busket-regular hide">
-                <button id="${e.id}" class="to__busket-minus"><i class="fas fa-minus"></i></button>
-                <input type="number" value="1" name="" id="${e.id}">
-                <button id="${e.id}" class="to__busket-plus"><i class="fas fa-plus"></i></button>
-            </div>
-          </div>
-        </div>
-                `;
-    });
-    this.setEventToAddButton();
-    this.setEventToPlusButtons();
-    this.setEvenToMinusButtons();
-  }
-
-  getBusket() {
-    if (getCookie("busket")) {
-      busket.innerHTML = ``;
-      this.busket.map((v) => {
-        this.vp = this.prodcuts[v.id - 1];
-        busket.innerHTML += `
-          <div id="${this.vp.id}" class="busket__item">
-            <button id="${this.vp.id}" class="remove">
-              <i class="fas fa-times"></i>
-            </button>
-            <img src="${this.vp.image}" alt="" />
-            <h4 class="title">${this.vp.title}</h4>
-            <h5 class="price">${this.vp.price} $</h5>
-            <input type="number" class="quanity" value="${v.count}">
-            <h5 class="sub__total">${this.vp.price * v.count} $</h5>
-          </div>
-                  `;
-      });
-      this.totalCostBusket.innerHTML = `
-      <h5>Total Cost: ${(+Math.abs(this.sumPrice)).toFixed(2)} $</h5>
-      `;
-      busket.appendChild(this.totalCostBusket);
-      this.setEventToRemoveButton();
-    }
-  }
-
-  getProduct(item) {
-    product.innerHTML = `
-      <div id="${item.id}" class="product__item">
-        <h4 class="title">${item.title}</h4>
-        <img src="${item.image}" alt="" />
-        <div class="more">
-          <p class="description">${item.description}</p>
-          <h5 class="price">${item.price} $</h5>
-          <div class="about">
-            <h6>
-              rating:
-              <p>${item.rating.rate}</p>
-            </h6>
-            <h6>
-              count:
-              <p>${item.rating.count}</p>
-            </h6>
-          </div>
-        </div>
-      </div>
-        `;
-  }
-
-  hashChange() {
-    if (location.hash === "#cart") {
-      main.classList.add("hide");
-      product.classList.add("hide");
-      busket.classList.toggle("hide");
-      this.getBusket();
-    } else if (
-      location.hash === `#product/${location.hash.split(`product/`)[1]}`
-    ) {
-      busket.classList.add("hide");
-      main.classList.add("hide");
-      product.classList.remove("hide");
-      this.item = this.prodcuts.find(
-        (item) => item.id == location.hash.split(`product/`)[1]
-      );
-      this.getProduct(this.item);
-    } else {
-      busket.classList.add("hide");
-      product.classList.add("hide");
-      main.classList.remove("hide");
-    }
-    window.scrollTo(0, 0)
+  setRemoteConfig() {
+    this.remoteConfig = [
+      {
+        div: main,
+        hash: "",
+        display: displayMain,
+      },
+      {
+        div: busket,
+        hash: "#cart",
+        display: displayBusket,
+      },
+      {
+        div: product,
+        hash: getDinHash('product'),
+        display: displayProduct,
+      },
+    ];
   }
 
   setEventHashChange() {
-    window.addEventListener("hashchange", () => this.hashChange());
-    // window.addEventListener('beforeunload', (this.hashChange)) // event перед загрузкой?
+    window.addEventListener("hashchange", () =>  {
+      this.setRemoteConfig();
+      hashChange(this.remoteConfig, this)
+    });
   }
+
   setEventToAddButton() {
     this.toBusketButtons = main.querySelectorAll(".to__busket-add");
     this.toBusketButtons.forEach((el) => {
@@ -228,7 +147,7 @@ export class App {
             (this.sumPrice -=
               this.prodcuts[el.id - 1].price * this.elCount).toFixed(2)
           ) + ` $`;
-        this.getBusket();
+        displayBusket(busket, this);
         setCookie("busket", this.busket);
       });
     });
@@ -252,9 +171,12 @@ export class App {
     this.create();
     this.render();
     await this.getFakeAPI();
+    this.setRemoteConfig()
     this.setEventHashChange();
-    this.getMain();
-    this.getBusket();
+    displayMain(main, this);
+    if (getCookie("busket")) {
+      displayBusket(busket, this);
+    }
     this.getBusketCount();
     this.getBusketTotalPrice();
   }
